dec168637d701cff139f7924de6bed4ff810b0ec
Kyle <klim385@aucklanduni.ac.nz>
2024-06-28 20:39:37 +1200

    Randomly generates opponent choiceonclick

--- files/script.js
+++ files/script.js
@@ -1,3 +1,14 @@
-function getUserChoice(choice) {
-  alert(`You chose ${choice}`);
+function getOpponentChoice() {
+  const choices = ["paper", "scissors", "rock"];
+  const randomIndex = Math.floor(Math.random() * choices.length); // Corrected Math.random() usage
+
+  return choices[randomIndex];
+}
+
+function playGame(userChoice) {
+  const opponentChoice = getOpponentChoice();
+  alert(
+    `You chose ${userChoice}
+    Computer chose ${opponentChoice}`
+  );
 }
