12dcae1f8b7c35bb8471f1cdbd86772679a61e48
Kyle <klim385@aucklanduni.ac.nz>
2024-06-29 18:52:01 +1200

    Code restructured: updateDisplay() updates scores and current round, selectors moved to top

--- files/script.js
+++ files/script.js
@@ -1,22 +1,18 @@
-// If page is refreshed, retrieve previous scores
-let score = JSON.parse(localStorage.getItem('score'));
-
-// If page is newly opened, initialise a score object
-if (!score) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
-  };
-}
-
-displayScores();
-
+// Selectors
+let scoresElement = document.getElementById("scores");
 let userChoiceElement = 
   document.getElementById("selected-user-choice-icon");
 let opponentChoiceElement = 
   document.getElementById("selected-opponent-choice-icon");
 let resultsElement = document.getElementById("results");
+
+score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+resetDisplay();
 
 function getOpponentChoice() {
   const choices = ["paper", "scissors", "rock"];
@@ -48,42 +44,35 @@ function playGame(userChoice) {
 
   // Display results and score on document
   let resultMessage = `<strong>${result}</strong>`;
-  resultsElement.innerHTML = resultMessage;
-
-  displayScores();
 
   // Display played moves
   let userChoiceIcon = `images/${userChoice}.png`;
   let opponentChoiceIcon = `images/${opponentChoice}.png`;
 
+  updateDisplay(resultMessage, userChoiceIcon, opponentChoiceIcon);
+}
+
+function updateDisplay(resultMessage, userChoiceIcon, opponentChoiceIcon) {
+  let scoresMessage = `Wins: ${score.wins}<br>
+  Losses: ${score.losses}<br>
+  Ties: ${score.ties}`
+  
+  scoresElement.innerHTML = scoresMessage;
+  resultsElement.innerHTML = resultMessage;
   userChoiceElement.src = userChoiceIcon;
   opponentChoiceElement.src = opponentChoiceIcon;
 }
 
-function displayScores() {
-  let scoresMessage = `Wins: ${score.wins}<br>
-  Losses: ${score.losses}<br>
-  Ties: ${score.ties}`
-  let scoresElement = document.getElementById("scores");
-  scoresElement.innerHTML = scoresMessage;
-}
-
-function resetScore() {
+function resetDisplay() {
   score.wins = 0;
   score.losses = 0;
   score.ties = 0;
 
   // Reset current round details on document
   let resultMessage = 'Press an option to start';
-  resultsElement.innerHTML = resultMessage;
-
-  // Reset scoreboard on document
-  displayScores();
-
-  // Reset current moves on document
   let userChoiceIcon = `images/blank.png`;
   let opponentChoiceIcon = `images/blank.png`;
 
-  userChoiceElement.src = userChoiceIcon;
-  opponentChoiceElement.src = opponentChoiceIcon;
+  // Reset display on document
+  updateDisplay(resultMessage, userChoiceIcon, opponentChoiceIcon);
 }
